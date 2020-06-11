b654658db68de5bf475790bf42e6731151abc0f6
cho881020 <kj_cho@idealidea.co.kr>
2020-06-11 20:11:01 +0900

    채팅 메세지 데이터 클래스 추가

---
+++ app/src/main/java/kr/co/tjoeun/numberbaseballgame_20200611/datas/Chat.kt
@@ -0,0 +1,9 @@
+package kr.co.tjoeun.numberbaseballgame_20200611.datas
+
+// 채팅은 누가? 뭐라고? 로 데이터 구성
+// who - USER / CPU 로 넣자.
+// content - 채팅으로 나타날 내용
+class Chat(
+    val who:String,
+    val content:String) {
+}
