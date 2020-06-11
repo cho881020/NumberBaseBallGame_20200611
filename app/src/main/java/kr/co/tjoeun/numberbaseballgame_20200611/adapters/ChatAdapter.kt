db67c8acef0eb85810730a6576f49e3137674010
cho881020 <kj_cho@idealidea.co.kr>
2020-06-11 20:46:47 +0900

    실제 채팅 내용을 상황에 따라 다른 텍스트뷰에 적용

--- app/src/main/java/kr/co/tjoeun/numberbaseballgame_20200611/adapters/ChatAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/numberbaseballgame_20200611/adapters/ChatAdapter.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.LinearLayout
+import android.widget.TextView
 import kr.co.tjoeun.numberbaseballgame_20200611.R
 import kr.co.tjoeun.numberbaseballgame_20200611.datas.Chat
 
@@ -25,6 +26,8 @@ class ChatAdapter(val mContext:Context, val resId:Int, val mList : List<Chat>) :
 
         val computerChatLayout = row.findViewById<LinearLayout>(R.id.computerChatLayout)
         val userChatLayout = row.findViewById<LinearLayout>(R.id.userChatLayout)
+        val computerChatTxt = row.findViewById<TextView>(R.id.computerChatTxt)
+        val userChatTxt = row.findViewById<TextView>(R.id.userChatTxt)
 
 //        이번에 뿌려줄 채팅 데이터 확인
         val data = mList[position]
@@ -35,11 +38,18 @@ class ChatAdapter(val mContext:Context, val resId:Int, val mList : List<Chat>) :
 //                사람 메세지 보여주고 / 컴퓨터 메세지는 숨김
                 userChatLayout.visibility = View.VISIBLE
                 computerChatLayout.visibility = View.GONE
+
+//                채팅 내용을 사람텍스트뷰에 적자
+                userChatTxt.text = data.content
             }
             "CPU" -> {
 //                컴퓨터쪽 보여주고 / 사람쪽 숨김
                 computerChatLayout.visibility = View.VISIBLE
                 userChatLayout.visibility = View.GONE
+
+//                채팅내용을 컴퓨터텍스트뷰에 적자
+                computerChatTxt.text = data.content
+
             }
             else -> {
                 Log.e("에러발생", "사용자 / 컴퓨터외의 화자 생김.")
