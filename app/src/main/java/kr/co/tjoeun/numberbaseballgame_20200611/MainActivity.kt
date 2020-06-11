cb9086800c532e0b5da362037a5a27f5b9696f07
cho881020 <kj_cho@idealidea.co.kr>
2020-06-11 19:40:20 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/numberbaseballgame_20200611/MainActivity.kt
@@ -0,0 +1,24 @@
+package kr.co.tjoeun.numberbaseballgame_20200611
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
